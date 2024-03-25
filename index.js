1c16e996ef4e1cb90f072e734442b2754986ac17
zain1891 <zainbrohi000@gmail.com>
2024-03-25 06:26:04 +0500

    contact us page complete

---
+++ index.js
@@ -0,0 +1,19 @@
+function myData(){
+
+    let username= document.getElementById("name").value
+    console.log(username);
+
+    event.preventDefault();
+    let userEmail= document.getElementById("email").value
+    console.log(userEmail);
+
+
+    let usernumber= document.getElementById("number").value
+    console.log(usernumber);
+
+    let usersubject= document.getElementById("subject").value
+    console.log(usersubject)
+
+    let usermessage= document.getElementById("meassage").value
+    console.log(usermessage)
+}
